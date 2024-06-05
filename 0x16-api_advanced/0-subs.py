0d4bc70c6c31d1533ebc26683df8ef9d6219fc00
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-06-05 02:09:46 +0100

    My commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,31 +1,19 @@
 #!/usr/bin/python3
-"""This modules houses the function that queries reddit API to obtain the
-total number of subscribers of a given subreddit. The subreddit's name is
-passed from the commandline, this application is a commandline application.
-"""
+""" module for function to return number of subreddit subscribers """
 import requests
+import sys
+
 
 def number_of_subscribers(subreddit):
-    """queries the subreddit api to obtain the total number
-    of subscriber of the passed subreddit
+    """  Args:
+        subreddit: subreddit name
+    Returns:
+        number of subscribers to the subreddit,
+        or 0 if subreddit requested is invalid"""
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    Args:
-        subreddit - the passed subreddit whose subscribers are obtained
-    """
-    base_url = 'https://www.reddit.com'
-    subreddit_api_url = '/r/none_subreddit/about'
-    if (subreddit):
-        subreddit_api_url = '/r/{}/about'.format(subreddit)
-    print(subreddit_api_url)
-    url = base_url + subreddit_api_url
-    print(url)
-    response = requests.get(url, allow_redirects=False)
-    print(response.status_code)
-
-    if(response.status_code == 200):
-        subscr_ind = response.text.index('subscribers')
-        subscr = response.text[subscr_ind:subscr_ind+50]
-        subscr = subscr.split('\n')[0].split('=')
-        return int(eval(subscr[1]))
-    else:
-        return 0
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
